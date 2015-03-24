b3a7c9b2dd46223822cea9ee69b5873004b93a4a
Edward D <edakin@wearearchitect.com>
2015-03-24 09:10:16 +0000

    feat(): Add in User languages

--- includes/class-wpml-editor-languages.php
+++ includes/class-wpml-editor-languages.php
@@ -25,7 +25,7 @@
  * @since      1.0.0
  * @package    Wpml_Editor_Languages
  * @subpackage Wpml_Editor_Languages/includes
- * @author     Your Name <email@example.com>
+ * @author     Edward D <edward@ozthegreat.io>
  */
 class Wpml_Editor_Languages {
 
@@ -44,9 +44,9 @@ class Wpml_Editor_Languages {
 	 *
 	 * @since    1.0.0
 	 * @access   protected
-	 * @var      string    $Wpml_Editor_Languages    The string used to uniquely identify this plugin.
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
 	 */
-	protected $Wpml_Editor_Languages;
+	protected $plugin_name;
 
 	/**
 	 * The current version of the plugin.
@@ -68,13 +68,12 @@ class Wpml_Editor_Languages {
 	 */
 	public function __construct() {
 
-		$this->Wpml_Editor_Languages = 'plugin-name';
+		$this->plugin_name = 'wpml-editor-languages';
 		$this->version = '1.0.0';
 
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -100,24 +99,18 @@ class Wpml_Editor_Languages {
 		 * The class responsible for orchestrating the actions and filters of the
 		 * core plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-plugin-name-loader.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpml-editor-languages-loader.php';
 
 		/**
 		 * The class responsible for defining internationalization functionality
 		 * of the plugin.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-plugin-name-i18n.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpml-editor-languages-i18n.php';
 
 		/**
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-plugin-name-admin.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-plugin-name-public.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wpml-editor-languages-admin.php';
 
 		$this->loader = new Wpml_Editor_Languages_Loader();
 
@@ -135,7 +128,7 @@ class Wpml_Editor_Languages {
 	private function set_locale() {
 
 		$plugin_i18n = new Wpml_Editor_Languages_i18n();
-		$plugin_i18n->set_domain( $this->get_Wpml_Editor_Languages() );
+		$plugin_i18n->set_domain( $this->get_plugin_name() );
 
 		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
 
@@ -150,26 +143,13 @@ class Wpml_Editor_Languages {
 	 */
 	private function define_admin_hooks() {
 
-		$plugin_admin = new Wpml_Editor_Languages_Admin( $this->get_Wpml_Editor_Languages(), $this->get_version() );
+		$plugin_admin = new Wpml_Editor_Languages_Admin( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Wpml_Editor_Languages_Public( $this->get_Wpml_Editor_Languages(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		$this->loader->add_action( 'init', $plugin_admin, 'set_allowed_languages' );
+		$this->loader->add_action( 'login_redirect', $plugin_admin, 'login_allowd_languages_redirect' );
+		$this->loader->add_action( 'edit_user_profile', $plugin_admin, 'add_user_languages_persmissions' );
+		$this->loader->add_action( 'personal_options_update', $plugin_admin, 'save_user_languages_allowed' );
+		$this->loader->add_action( 'edit_user_profile_update', $plugin_admin, 'save_user_languages_allowed' );
 
 	}
 
@@ -189,8 +169,8 @@ class Wpml_Editor_Languages {
 	 * @since     1.0.0
 	 * @return    string    The name of the plugin.
 	 */
-	public function get_Wpml_Editor_Languages() {
-		return $this->Wpml_Editor_Languages;
+	public function get_plugin_name() {
+		return $this->plugin_name;
 	}
 
 	/**
